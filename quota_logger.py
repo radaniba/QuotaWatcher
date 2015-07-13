e2f06e1d023ae7017134bd21af419c709799cceb
Rad Aniba <raniba@momac36.bccrc.ca>
2015-07-13 10:36:42 -0700

    Added support for logstash_formatter In case you want to export your results into another pipeline let's say ELK stasck, you can use this commit to output your results as a json format and feed it to elasticsearch for example

--- quota_logger.py
+++ quota_logger.py
@@ -1,14 +1,17 @@
 import logging
 import datetime
+from logstash_formatter import LogstashFormatter
+import logging as log
 
 def init_log():
     current_time = datetime.datetime.now()
-    logger = logging.getLogger('__name__')
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(current_time.isoformat()+'_quotawatcher.log')
-    handler.setLevel(logging.INFO)
+    logger = log.getLogger(__name__)
+    logger.setLevel(log.INFO)
+    handler = log.FileHandler('quota_watcher')
+    handler.setLevel(log.INFO)
     # create a logging format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    #formatter = log.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = LogstashFormatter()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
